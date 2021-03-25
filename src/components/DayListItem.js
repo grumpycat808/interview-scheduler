705037e7433fe96fc4a01a6a978455036b3c1d13
Catherine Li <catherine.l@intiveo.com>
2021-03-25 15:59:51 -0700

    Added conditional styles to the DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classnames from 'classnames/bind';
 
 export default function DayListItem(props) {
-  return (
-    <li>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
-    </li>
-  );
+    let listItemClass = classnames({
+        'day-list__item': true,
+        'day-list__item--selected': props.selected,
+        'day-list__item--full': props.full
+     })  
+    return (
+        <li className={listItemClass}>
+            <h2>{props.name}</h2> 
+            <h3>{props.spots} spots remaining</h3>
+        </li>
+    );
 }

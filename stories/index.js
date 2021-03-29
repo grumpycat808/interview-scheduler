2e1430d5b4fd5dabac23e274011c11da2c8fe6b3
Catherine Li <catherine.l@intiveo.com>
2021-03-29 10:45:39 -0700

    Create Empty appointment fragment

--- stories/index.js
+++ stories/index.js
@@ -4,12 +4,16 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
-
+import "components/Appointments/styles.scss"
 import Button from "components/Button";
 import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewListItem"
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointments"
+import Header from "components/Appointments/Header"
+import Empty from "components/Appointments/Empty"
+import Show from "components/Appointments/Show"
 
 storiesOf("Button", module)
   .addParameters({
@@ -125,3 +129,13 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+.addParameters({
+  backgrounds: [{ name: "white", value: "#fff", default: true }]
+})
+.add("Appointment", () => <Appointment />)
+.add("Appointment with Time", () => <Appointment time="12pm" />)
+.add("Header", () => <Header time="12pm" />)
+.add("Empty", () => <Empty onAdd={action("onAdd")} />)
+.add("Show", () => <Show student="" interviewer="" onEdit={action("onEdit")} onDelete={action("onDelete")} />);
